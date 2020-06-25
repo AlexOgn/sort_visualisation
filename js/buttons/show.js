645f208e00e0bd6e84a0f5e8f1b2e6db15338ba6
AlexOgn <61375132+AlexOgn@users.noreply.github.com>
2020-06-25 12:02:12 +0300

    Added images and "For us" page

--- js/buttons/show.js
+++ js/buttons/show.js
@@ -1,23 +1,31 @@
 function show_options(id_button, id_div, pause, resume) {
-    let x = document.getElementById(id_div);
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    let а = document.getElementById(id_div);
+    if (а.style.display === "none") {
+      а.style.display = "block";
       document.getElementById(id_button).innerHTML = "Скриване на Визуализацията";
       resume();
     } else {
-      x.style.display = "none";
+      а.style.display = "none";
       document.getElementById(id_button).innerHTML = "Показване на Визуализацията";
       pause();
     }
 }
 function show_text(id_button, id_div) {
-  let y = document.getElementById(id_div);
-  if (y.style.display === "none") {
-    y.style.display = "block";
+  let а = document.getElementById(id_div);
+  if (а.style.display === "none") {
+    а.style.display = "block";
     document.getElementById(id_button).innerHTML = "Скриване на Информацията";
   } else {
-    y.style.display = "none";
+    а.style.display = "none";
     document.getElementById(id_button).innerHTML = "Показване на Информацията";
   }
 }
 
+function show_creators(id_ul){
+  let а = document.getElementById(id_ul);
+  if (а.style.display === "none") {
+    а.style.display = "block";
+  } else {
+    а.style.display = "none";
+  }
+}
